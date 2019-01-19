477c6aff932914b924c1a6d408bb0af7a742a55d
amarufd <amarud@gmail.com>
2019-01-19 01:45:40 -0300

    se agrega construccion de data frame final

--- final final.R
+++ final final.R
@@ -1,8 +1,15 @@
+# Libreria encargada de leer el html
+library(rvest)
+
+# DataFrame encargado de almacenar la informacion
+datosJuntos = data.frame()
+
+
 # comentarios profe
 for(i in 1:3){
   print(paste("http://www.garrasypatas.cl/perros/page/",i,"/",sep = ""))
   
-  read_html(paste("http://www.garrasypatas.cl/perros/page/",i,"/",sep = ""))
+  web_perros <- read_html(paste("http://www.garrasypatas.cl/perros/page/",i,"/",sep = ""))
   
   los_link_perros <- html_nodes(web_perros, '.cmsms_open_link')
   
@@ -17,14 +24,26 @@ for(i in 1:3){
     primer_perro<- read_html(adopta_perros)
     
     nombre <- html_nodes(primer_perro,'.project_features_item_title')
-    limpio <- html_text(nombre)
-    tabla1<- as.data.frame(limpio)
+    
+    # Extraccion del texto contenido en otra_tabla con la función html_text
+    # y transformación a lista de los contenidos con as.list
+    limpio <- as.list(html_text(nombre))
     
     otra_tabla <- html_nodes(primer_perro,'.project_features_item_desc' )
-    tabla_limpia <- html_text (otra_tabla)
-    tabla2<-as.data.frame (tabla_limpia)
     
-    tablajunta <- data.frame(tabla1,tabla2)
+    # Extraccion del texto contenido en otra_tabla con la función html_text
+    # y transformación a lista de los contenidos con as.list
+    tabla_limpia <- as.list(html_text(otra_tabla))
+    
+    # Asignación de nombres a la lista tabla_limpia, que se encuentran
+    # almacenados en la variable limpio
+    names(tabla_limpia) <- limpio
+    
+    # Creacio de dataframe a partir de los datos de la variable tabla_limpia
+    tablajunta <- as.data.frame(tabla_limpia)
+    
+    # Union de dataFrames
+    datosJuntos <- rbind(datosJuntos,tablajunta)
     
   }
 }
